e57e8baa5966a6860d5c7fb466295c815e72646d
Jocel <jocel.junior.silva@gmail.com>
2023-07-31 16:59:56 -0300

    test2

--- lib/app/view/crew/widgets/crew_item_widget.dart
+++ lib/app/view/crew/widgets/crew_item_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../../models/crew.dart';
 
 class CrewItemWidget extends StatelessWidget {
@@ -60,13 +60,6 @@ class CrewItemWidget extends StatelessWidget {
   }
 
   void abrirWhatsApp(String message) async {
-    var whatsappUrl =
-        "https://whatsapp.com/send?text=${Uri.encodeComponent(message)}";
-
-    if (await canLaunchUrl(Uri.parse(whatsappUrl))) {
-      await launchUrl(Uri.parse(whatsappUrl));
-    } else {
-      throw 'Could not launch $whatsappUrl';
-    }
+    Share.share(message);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
-import url_launcher_macos
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
